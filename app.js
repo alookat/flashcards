7c187b06e10158789dc644884e7115bdd321038e
Danny White <white.daniel.james@gmail.com>
2017-03-14 17:44:40 -0400

    Commented

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ const revealButton = document.querySelector("button#reveal");
 const editButton = document.querySelector("button#edit");
 
 
-
-
 // Start with inability to reset
 resetButton.setAttribute("disabled");
 
@@ -32,7 +30,7 @@ let spanStyles = {
   }
 }
 
-// Loop over split after-translation
+// Create function to loop over after-translation string
 function splitWords(string, parent) {
   // Store the split after-translation string
   let words = string.split(" ");
@@ -71,9 +69,11 @@ function splitWords(string, parent) {
 
 // Set the first paragraph as the before-translation string
 engP.textContent = engText;
+
 // Set the second paragraph as the after-translation string, already split
 splitWords(espText, espP);
 
+// Detect and act on clicks on spans
 // Bubble up to spans to hide/show indivual reveals/hides
 section.addEventListener("click", (e) => {
   if (e.target.tagName === "SPAN") {
@@ -83,69 +83,68 @@ section.addEventListener("click", (e) => {
     if (shown === false) {
   		resetButton.removeAttribute("disabled");
       spanStyles.shown(selectSpan);
-
       shown = true;
     } else if (shown === true) {
       resetButton.setAttribute("disabled");
       spanStyles.covered(selectSpan);
-
       shown = false;
     }
   }
 });
 
 
-// DANGER ZONE
+// Re-do all of this if the edit button is clicked, and then clicked again (saved)
 editButton.addEventListener("click", (e) => {
+  // Normal to Edit mode
 	if (editButton.textContent === "Edit") {
+    // Prepare button for switch back to Save(d)
     editButton.textContent = "Save";
 
+    // Disable all flashcards resetting/revealing whilst editing
     resetButton.setAttribute("disabled");
     revealButton.setAttribute("disabled");
 
+    // Create input, fill with existing P content, remove P
+    // Refactor me
 		const engInput = document.createElement("input");
-		const espInput = document.createElement("input");
-
     const engP = document.getElementById("eng-p");
-    const espP = document.getElementById("esp-p");
-
 		engInput.type = "text";
 		engInput.value = engP.textContent;
 		section.insertBefore(engInput, engP);
 		section.removeChild(engP);
-
+    // Same as above
+    const espInput = document.createElement("input");
+    const espP = document.getElementById("esp-p");
 		espInput.type = "text";
 		espInput.value = espP.textContent;
 		section.insertBefore(espInput, espP);
 		section.removeChild(espP);
-
-
-
-	} else if (editButton.textContent === "Save") {
+	}
+  // Edit mode to Normal
+  else if (editButton.textContent === "Save") {
+    // Prepare button for switch back to Edit
     editButton.textContent = "Edit";
 
+    // Re-enable resetting and revealing of flashcards
     resetButton.removeAttribute("disabled");
     revealButton.removeAttribute("disabled");
+    // Disable reset for now, since flashcards will start covered
     resetButton.setAttribute("disabled");
 
+    // Create P, fill with existing input content, remove input
+    // Refactor me
 		const engInput = section.firstElementChild;
 		const engP = document.createElement("p");
     engP.setAttribute("id", "eng-p");
     engP.textContent = engInput.value;
-
     section.insertBefore(engP, engInput);
     section.removeChild(engInput);
-
-
+    // *Similar* to above
     const espInput = section.firstElementChild.nextElementSibling;
 		let espP = document.createElement("p");
     espP.setAttribute("id", "esp-p");
-
     splitWords(espInput.value, espP);
-
     section.insertBefore(espP, espInput);
 		section.removeChild(espInput);
-
 	}
-
 });
