c8f35e18b76ed2d1ec535f92a241f3191058443f
Danny White <white.daniel.james@gmail.com>
2017-03-14 16:14:11 -0400

    Pre Refactor

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const resetButton = document.querySelector("button#reset");
 const revealButton = document.querySelector("button#reveal");
 const editButton = document.querySelector("button#edit");
 
-
-
 const espWords = espText.split(" ");
 
 engP.textContent = engText;
@@ -64,43 +62,103 @@ for (let i = 0; i < espWords.length; i += 1) {
 }
 
 editButton.addEventListener("click", (e) => {
-	let edit = false;
 	if (editButton.textContent === "Edit") {
+    console.log("edit button clicked")
 		const engInput = document.createElement("input");
 		const espInput = document.createElement("input");
+
+    // const section = editButton.parentNode;
+    const engP = document.getElementById("eng-p");
+    const espP = document.getElementById("esp-p");
 
 		engInput.type = "text";
 		engInput.value = engP.textContent;
 		section.insertBefore(engInput, engP);
 		section.removeChild(engP);
-
+    // console.log(engP.parentNode);
+    //
 		espInput.type = "text";
 		espInput.value = espP.textContent;
 		section.insertBefore(espInput, espP);
 		section.removeChild(espP);
 
 		editButton.textContent = "Save";
-		return edit = true;
 
 	} else if (editButton.textContent === "Save") {
+    console.log("save button clicked")
 		const engInput = section.firstElementChild;
 		const espInput = section.firstElementChild.nextElementSibling;
 
+    // console.log(engInput, espInput);
+    //
 		const engP = document.createElement("p");
+    engP.setAttribute("id", "eng-p");
+
 		const espP = document.createElement("p");
-
+    espP.setAttribute("id", "esp-p");
+    //
 		engP.textContent = engInput.value;
-		espP.textContent = espInput.value;
-
+		// espP.textContent = espInput.value;
+    //
 		section.insertBefore(engP, engInput);
-		section.insertBefore(espP, espInput);
+    section.removeChild(engInput);
+    //
 
-		section.removeChild(engInput);
-		section.removeChild(espInput);
 
 		editButton.textContent = "Edit";
 
-		return edit = false;
+    const espWords = espInput.value.split(" ");
+
+    for (let i = 0; i < espWords.length; i += 1) {
+      let espSpan = document.createElement("span");
+      let espSpanSpace = document.createElement("span")
+
+      espSpanSpace.textContent = " ";
+      espSpan.style.backgroundColor = "white";
+      espSpan.style.color = "white";
+      espSpan.style.boxShadow = "0 3px 6px #ccc";
+      espSpan.textContent = espWords[i];
+      espP.appendChild(espSpan);
+      espP.appendChild(espSpanSpace);
+      let shown = false;
+
+      espSpan.addEventListener("click", (e) => {
+        if (shown === false) {
+    			resetButton.removeAttribute("disabled");
+          espSpan.style.backgroundColor = "";
+          espSpan.style.color = "";
+          espSpan.style.boxShadow = "";
+          shown = true;
+        } else if (shown === true) {
+          espSpan.style.backgroundColor = "white";
+          espSpan.style.color = "white";
+          espSpan.style.boxShadow = "0 3px 6px #ccc"
+          shown = false;
+        }
+      });
+
+      resetButton.addEventListener("click", (e) => {
+        espSpan.style.backgroundColor = "white";
+        espSpan.style.color = "white";
+        espSpan.style.boxShadow = "0 3px 6px #ccc";
+        shown = false;
+    		resetButton.setAttribute("disabled");
+    		revealButton.removeAttribute("disabled");
+      });
+
+      revealButton.addEventListener("click", (e) => {
+        espSpan.style.backgroundColor = "";
+        espSpan.style.color = "";
+        espSpan.style.boxShadow = "";
+        shown = true;
+    		revealButton.setAttribute("disabled");
+    		resetButton.removeAttribute("disabled");
+      });
+    }
+
+    section.insertBefore(espP, espInput);
+		section.removeChild(espInput);
+
 	}
 
 });
